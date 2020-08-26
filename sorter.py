5b481ac6698e92cca5d28b0b09fe05f4a706d0e1
DensonAbraham <densonabraham98@gmail.com>
2020-08-26 23:08:22 +0530

    Added support for large files

--- sorter.py
+++ sorter.py
@@ -3,19 +3,30 @@ from watchdog.events import FileSystemEventHandler
  
 import os 
 import json  
-import time   
 
-class myfilehandler(FileSystemEventHandler):   
-    def on_modified(self,event):   
-        for filename in os.listdir(mother_folder):
-            src = mother_folder + "/" + filename  
+
+class myfilehandler(FileSystemEventHandler):    
+    def on_modified(self,event):    
+        onlyfiles = [f for f in os.listdir(mother_folder) ]    
+        listOfFiles=[]     
+        for file in onlyfiles: 
+            if len(file.split("."))>1:
+                historicalSize = -1
+                while (historicalSize != os.path.getsize(mother_folder + "/" + file)):
+                    historicalSize = os.path.getsize((mother_folder + "/" + file))
+                    print(".....loading......",historicalSize)
+                    time.sleep(1)
+                listOfFiles.append(file) 
+
+        for filename in listOfFiles:
+            src = mother_folder + "/" + filename    
             new_sorted= folder_dest + "/" + filename  
             os.rename(src , new_sorted)  
 
-# mother_folder -> folder under observation  
+# mother_folder -> folder under observation  , path to the folder
 # folder_dest -> Folder to which files will be copied
-mother_folder = "/home/pimple/Documents/COding/python_file_sorter"     
-folder_dest = "/home/pimple/Documents/COding/read"  
+mother_folder = "/home/pimple/Documents/sorter"     
+folder_dest = "/home/pimple/Documents/read"  
 event_handler = myfilehandler()   
 observer = Observer()   
 observer.schedule(event_handler , mother_folder,recursive = True)  
